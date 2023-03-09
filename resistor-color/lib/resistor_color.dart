7929c310e26561b348e35ed21884905f345aee42
Gaspar Habif <gasparhabif@gmail.com>
2023-03-08 21:21:34 -0300

    resistor-color resolved

--- resistor-color/lib/resistor_color.dart
+++ resistor-color/lib/resistor_color.dart
@@ -1,3 +1,17 @@
+const Map<String, int> resistors = {
+  "black": 0,
+  "brown": 1,
+  "red": 2,
+  "orange": 3,
+  "yellow": 4,
+  "green": 5,
+  "blue": 6,
+  "violet": 7,
+  "grey": 8,
+  "white": 9
+};
+
 class ResistorColor {
-  // Put your code here
+  List<String> get colors => resistors.keys.toList();
+  int colorCode(String key) => resistors[key]!;
 }
